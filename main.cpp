007cbe2bd1a64a089c57e6ad295a40cf63e5d3c6
Ahmed ibarhim <ahmedibrahim55518@gmail.com>
2023-11-25 00:52:53 +0200

    Update Functions

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <rectangle.h>
 using namespace std;
-
+int ans=0;
+void text(){
+    ans=1;
+}
 int main() {
     rectangle a(1,4);
     cout << a.getY() << endl;
+    text();
+    cout<<ans<<"\n";
     return 0;
 }
